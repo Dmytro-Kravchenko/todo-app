74b0ded2598fde57260d7d7ada25ec987c873185
Dmytro Kravchenko <dmytro318@gmail.com>
2024-04-12 20:25:02 +0300

    todo app

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
-# React TypeScript Starter Pack
+# Todo App
+The Todo App is a simple application that helps you keep track of your tasks and manage your to-do list. With this app, you can easily add, edit, and delete tasks, mark them as completed, and organize them into different categories or priorities. Stay organized and never forget an important task again with the Todo App!
 
-To use this template click `Use this template`
+## Technologies that were used
+- HTML5
+- CSS3
+- Sass (SCSS)
+- TypeScript
+- React
 
-### Available Scripts
+# Links
+- [PREVIEW LINK](https://dmytro-kravchenko.github.io/todo-app/)
+- [REPOSITORY LINK](https://github.com/Dmytro-Kravchenko/todo-app)
 
-`Deploy` - available to deploy your application to gh-pages
+## How to launch the app locally
+To launch the Todo App on your local machine, follow these steps:
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+1. Clone the repository to your local machine: `git clone https://github.com/your-username/todo-app.git`
+
+2. Navigate to the project directory: `cd todo-app`
+
+3. Install the required dependencies: `npm install`
+
+4. Start the development server: `npm start`
+
+5. Open your web browser and visit `http://localhost:3000` to access the Todo App.
+
+Note: Make sure you have Node.js and npm installed on your machine before running the above commands.
+
+
+Enjoy using the Todo App and stay organized with your tasks!
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useEffect } from 'react';
+import { UserWarning } from './UserWarning';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { USER_ID, getTodos } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { TodoFooter } from './components/TodoFooter';
+import { useTodosContext } from './utils/useTodosContext';
+import { TodoHeader } from './components/TodoHeader';
+import { ErrorNotification } from './components/ErrorNotification';
+import { onErrors } from './utils/onErrors';
+import { Errors } from './enums/Errors';
 
 export const App: React.FC = () => {
+  const { todos, setTodos, setErrorMessage, preparedTodos } = useTodosContext();
+
+  useEffect(() => {
+    setErrorMessage(null);
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        onErrors(Errors.LoadTodos, setErrorMessage);
+      });
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader />
+
+        <TodoList todos={preparedTodos} />
+        {!!todos.length && <TodoFooter />}
+      </div>
+
+      <ErrorNotification />
     </div>
   );
 };
